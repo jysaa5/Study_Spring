bb9e4e96078936ef9d018bec32923e8e7591573c
jysaa5 <jysaa5@naver.com>
2021-07-04 23:49:32 +0900

    Study Spring

---
+++ LILAC-spring/src/main/java/LILAC/LILACspring/LilacSpringApplication.java
@@ -0,0 +1,13 @@
+package LILAC.LILACspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilacSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilacSpringApplication.class, args);
+	}
+
+}
---
+++ LILAC-spring/src/test/java/LILAC/LILACspring/LilacSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package LILAC.LILACspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilacSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
