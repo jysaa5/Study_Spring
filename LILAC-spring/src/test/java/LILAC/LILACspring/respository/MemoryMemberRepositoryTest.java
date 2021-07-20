b81be0aa8545418e2b9c43e2ff5a05a5327e578a
jysaa5 <jysaa5@naver.com>
2021-07-21 00:52:40 +0900

    Update MemoryMemberRepositoryTest.java

--- LILAC-spring/src/test/java/LILAC/LILACspring/respository/MemoryMemberRepositoryTest.java
+++ LILAC-spring/src/test/java/LILAC/LILACspring/respository/MemoryMemberRepositoryTest.java
@@ -6,6 +6,8 @@ import LILAC.LILACspring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class MemoryMemberRepositoryTest {
 
     MemberRepository repository = new MemoryMemberRepository();
@@ -18,7 +20,40 @@ class MemoryMemberRepositoryTest {
         repository.save(member);
         Member result = repository.findById(member.getId()).get();
         // System.out.println("result = " + (result == member));
-        // Assertions.assertEquals(member, result);
-        Assertions.assertThat(member).isEqualTo(result);
+        // Assertions.assertEquals(result, member);
+        Assertions.assertThat(result).isEqualTo(member);
+    }
+
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result1 = repository.findByName("spring1").get();
+
+        Assertions.assertThat(result1).isEqualTo(member1);
+
+    }
+
+    @Test
+    public void findAll(){
+        Member member3 = new Member();
+        member3.setName("spring1");
+        repository.save(member3);
+
+
+        Member member4 = new Member();
+        member4.setName("spring2");
+        repository.save(member4);
+
+        List<Member> result2 = repository.findAll();
+
+        Assertions.assertThat(result2.size()).isEqualTo(2);
     }
 }
