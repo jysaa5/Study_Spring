dcebc743fc4295a4114bb18ad1bf1e780c3217ac
jysaa5 <jysaa5@naver.com>
2021-08-23 23:39:51 +0900

    Update MemberServiceTest.java

--- LILAC-spring/src/test/java/LILAC/LILACspring/service/MemberServiceTest.java
+++ LILAC-spring/src/test/java/LILAC/LILACspring/service/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package LILAC.LILACspring.service;
 
 import LILAC.LILACspring.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,9 +15,14 @@ class MemberServiceTest {
         // given
         Member member = new Member();
         member.setName("hello");
+
         // when
+        Long saveId = memberService.join(member);
+
 
         // then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
     @Test
